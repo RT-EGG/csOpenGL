7a712260976730758b6e32d84e47921b04ae3296
RT-EGG <rt.egg.jp@gmail.com>
2019-04-26 17:37:19 +0900

    Add custom NumericUpDown control.

---
+++ rtUtility/Control/CustomNumericUpDown.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rtUtility.Control
+{
+    public partial class CustomNumericUpDown : NumericUpDown
+    {
+        public CustomNumericUpDown()
+        {
+            InitializeComponent();
+        }
+
+        public CustomNumericUpDown(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        public override string Text
+        {
+            get { return base.Text; }
+            set { base.Text = value + Tail; }
+        }
+
+        public string Tail
+        {
+            get { return p_Tail; }
+            set
+            {
+                if (p_Tail != value) {
+                    p_Tail = value;
+                    UpdateEditText();
+                    return;
+                }
+            }
+        }
+
+        private string p_Tail = "";
+    }
+}
