21db1dfd5fce232ecdd348d4df553c1bf7695b3a
RT-EGG <rt.egg.jp@gmail.com>
2019-07-20 11:49:42 +0900

    Update GLAttribStack.cs

--- rtOpenTK/GLAttribStack.cs
+++ rtOpenTK/GLAttribStack.cs
@@ -1,17 +1,67 @@
 ﻿// System
+using System;
 using System.Collections.Generic;
 // OpenTK
 using OpenTK.Graphics.OpenGL4;
+// rtUtility
+using rtUtility.Collections;
 
 namespace rtOpenTK
 {
-    public class TGLAttribStack
+    public enum TGLAttribGroup
     {
-        public TGLEnableAttribStack EnableBit
-        { get; } = new TGLEnableAttribStack();
+        EnableAttrib = 1,
+        AllAttrib = 0xFFFF
     }
 
-    public class TGLEnableAttribStack
+    public class TGLAttribStack
+    {
+        public TGLAttribStack()
+        {
+            groups.Add(new KeyValuePair<TGLAttribGroup, IGLAttribGroupStack>(TGLAttribGroup.EnableAttrib, EnableGroup));
+            return;
+        }
+
+        public void PushAttrib(TGLAttribGroup aAttrib)
+        {
+            foreach (var group in groups) {
+                if ((aAttrib & group.Key) != 0)
+                    group.Value.Push();
+            }
+            return;
+        }
+
+        public void PopAttrib(TGLAttribGroup aAttrib)
+        {
+            foreach (var group in groups) {
+                if ((aAttrib & group.Key) != 0)
+                    group.Value.Pop();
+            }
+            return;
+        }
+
+        public TGLEnableAttribStack EnableGroup
+        { get; } = new TGLEnableAttribStack();
+
+        public IGLAttribGroupStack GetAttribGroupStack(TGLAttribGroup aAttrib)
+        {
+            switch (aAttrib) {
+                case TGLAttribGroup.EnableAttrib:
+                    return EnableGroup;
+            }
+            return null;
+        }
+
+        private IList<KeyValuePair<TGLAttribGroup, IGLAttribGroupStack>> groups = new List<KeyValuePair<TGLAttribGroup, IGLAttribGroupStack>>();
+    }
+
+    public interface IGLAttribGroupStack
+    {
+        void Push();
+        void Pop();
+    }
+
+    public class TGLEnableAttribStack : IGLAttribGroupStack
     {
         public TGLEnableAttribStack()
         {
