6385185d73baab983f2900cc7f1cf5c810c330c0
r-takeuchi-prd <takeuchi@pocket-rd.com>
2020-03-26 22:39:42 +0900

    Add VertexArrayObject resource class.

---
+++ rtOpenTK/rtGLResourceObject/GLVertexArrayObject.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+namespace rtOpenTK.rtGLResourceObject
+{
+    public class TGLVertexArrayObject : TGLResourceObject
+    {
+        protected override void DoCreateGLResource(TrtGLControl aGL)
+        {
+            base.DoCreateGLResource(aGL);
+
+            ID = GL.GenVertexArray();
+            return;
+        }
+
+        protected override void DoDisposeGLResource(TrtGLControl aGL)
+        {
+            base.DoDisposeGLResource(aGL);
+
+            GL.DeleteVertexArray(ID);
+            return;
+        }
+
+        public override bool IsResourceReady => ID != 0;
+
+        public int ID
+        { get; private set; } = 0;
+    }
+}
